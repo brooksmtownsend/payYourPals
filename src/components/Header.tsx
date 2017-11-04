a27dc7c9535f61d3e018520b312a782690ba102e
Brooks Townsend <brooksmtownsend@gmail.com>
2017-11-04 17:36:57 -0400

    Added Facebook login to header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react'
 import '../styles/Header.css'
+import FacebookLogin from 'react-facebook-login'
+
+const responseFacebook = (response) => {
+  console.log(response);
+}
 
 export default class Header extends React.Component {
 
@@ -7,7 +12,21 @@ export default class Header extends React.Component {
     return (
       <div className="Header">
         <h1>Pay Your Pals</h1>
+        <div className="FacebookLogin">
+          <FacebookLogin 
+            appId='139629923459386'
+            autoLoad={true}
+            fields='name, email, picture'
+            scope='public_profile, user_friends'
+            callback={responseFacebook} 
+          />
+        </div>
       </div>
     )
   }
 }
+
+/**
+ * Documentation for the FacebookLogin component:
+ * https://www.npmjs.com/package/react-facebook-login
+ */
