55fb7c27144891a1e2a21ac8a16192574d72342d
Hank Henry Hester <hankh@live.unc.edu>
2017-11-04 15:24:41 -0400

    added header and footer components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react'
 import '../styles/App.css'
-import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
-      <Router>
       <div className="App">
         <div className="App-header">
           <h2>Welcome to React</h2>
@@ -14,7 +12,6 @@ class App extends React.Component {
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
       </div>
-      </Router>
     );
   }
 }
