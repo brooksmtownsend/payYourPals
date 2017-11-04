d4a8e28c89fa6807d294a5809f9cb396d640006b
Brooks Townsend <brooksmtownsend@gmail.com>
2017-11-04 15:32:29 -0400

    Persistent header and footer, and simple beginning to welcome & create event page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,31 @@
 import * as React from 'react'
 import '../styles/App.css'
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
       <Router>
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+        {/* <Header /> */}
+        {/* <Route path={'/'} component={WelcomePage} /> */}
+        {/* <Route path={'/createEvent'} component={CreateEventPage} /> */}
+        {/* <Footer /> */}
       </Router>
     );
   }
 }
 
 export default App;
+
+class Test extends React.Component<{match: any} , {}> {
+  render() {
+    let text = this.props.match.params.additionalParams
+    return (
+      <div>
+        <h1>{text}</h1>
+      </div>
+    )
+  }
+}
+
+// Link tags for navigating to other pages (can be buttons etc)
