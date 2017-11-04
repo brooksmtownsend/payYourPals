264d71177e632e5466d07065b5fa3a1770af3bb5
Hank Henry Hester <hankh@live.unc.edu>
2017-11-04 15:44:42 -0400

    fixed router setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './components/App';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
-import registerServiceWorker from './registerServiceWorker';
-import './styles/index.css';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import WelcomePage from './components/WelcomePage'
+import registerServiceWorker from './registerServiceWorker'
+import './styles/index.css'
 
 ReactDOM.render((
-  <Router>
-    <App />
-  </Router>
+    <Router>
+      <div>
+        <Header />
+        <Route path={'/'} component={WelcomePage} />
+        {/* <Route path={'/createEvent'} component={CreateEventPage} /> */}
+        <Footer />
+      </div>
+    </Router>
   ), document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
