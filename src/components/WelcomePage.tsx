e9c8449a34084cdf728d847135e2b4b3b67f357c
Hank Henry Hester <hankh@live.unc.edu>
2017-11-05 01:48:19 -0400

    updated welcome page

--- src/components/WelcomePage.tsx
+++ src/components/WelcomePage.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react'
 import '../styles/WelcomePage.css'
 
+const feature1Img = require('../images/feature1.png')
+const feature2Img = require('../images/feature2.png')
+const feature3Img = require('../images/feature3.png')
+
 export default class WelcomePage extends React.Component {
 
   render() {
@@ -10,7 +14,8 @@ export default class WelcomePage extends React.Component {
         <h1>Pay Your Pals</h1>
         <div className="topContent">
           <div className="left">
-            <h2>Create an event</h2>
+            <h2>Get people to pay for your events without the hassle</h2>
+            <h2>Commit to events without fear of overpaying</h2>
             <h2></h2>
           </div>
           <div className="right">
@@ -21,7 +26,7 @@ export default class WelcomePage extends React.Component {
         <div className="lowerContent">
           <div className="featureRow">
             <div className="featureIconContainer">
-              <div className="featureIcon"></div>
+              <img src={feature1Img} />
             </div>
             <div className="featureText">
               Afraid to host an event because you don't know if people will pay you back?
@@ -34,12 +39,12 @@ export default class WelcomePage extends React.Component {
               Pay Your Pals allows you to specify the maximum amount you are willing to pay!
             </div>
             <div className="featureIconContainer">
-              <div className="featureIcon"></div>
+              <img src={feature2Img} />
             </div>
           </div>
           <div className="featureRow">
             <div className="featureIconContainer">
-              <div className="featureIcon"></div>
+              <img src={feature3Img} />
             </div>
             <div className="featureText">
               Pay Your Pals removes financial uncertainty from an event whether you are the host or the attendee
