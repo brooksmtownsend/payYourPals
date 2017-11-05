37cd058ac34d1b6ac94170222af86e497048cd27
Hank Henry Hester <hankh@live.unc.edu>
2017-11-05 00:18:33 -0400

    added api proxy

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,12 +7,12 @@ const responseFacebook = (response) => {
   console.log(response);
 }
 
-export default class Header extends React.Component {
+export default class Header extends React.Component<{hideTitle?: boolean}, {}> {
 
   render() {
     return (
       <div className="Header">
-        <h1>Pay Your Pals</h1>
+        {this.props.hideTitle || <h1>Pay Your Pals</h1>}
         <div className="FacebookLogin">
           <FacebookLogin 
             appId='139629923459386'
