75c6497fbbed0c7a86e7b8b9fa4dd87cd599c81f
Brooks Townsend <brooksmtownsend@gmail.com>
2017-11-05 03:19:09 -0500

    Removed secret keys for softheon even though we don't use em

--- moneyStuff/getId.sh
+++
@@ -1,7 +0,0 @@
-curl -X POST \
-  https://hack.softheon.io/oauth2/connect/token \
-  -H 'authorization: Basic NzAxNjVhOGItMjAyNi00ZmFhLTg3ZGItODQwN2Y1YWVlYTk3OjE2MTNmYTliLTQ3YTMtNDYxNi1iOTQ4LWEwNWU4Y2VhZjc1ZA==' \
-  -H 'cache-control: no-cache' \
-  -H 'content-type: application/x-www-form-urlencoded' \
-  -H 'postman-token: 830102ce-7699-e737-f1cd-64d519e4c9e9' \
-  -d 'grant_type=client_credentials&scope=paymentapi'
